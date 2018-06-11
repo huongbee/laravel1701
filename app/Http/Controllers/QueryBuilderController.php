d81fefcfb7e446fd97425535a91016d235107dcf
Huong <admin@Apples-MacBook-Pro.local>
2018-06-11 19:24:36 +0700

    query builder

--- app/Http/Controllers/QueryBuilderController.php
+++ app/Http/Controllers/QueryBuilderController.php
@@ -59,13 +59,36 @@ class QueryBuilderController extends Controller
         //             })
         //             ->get();
 
+        // $products = DB::table('products')
+        //             ->selectRaw('categories.name as TenLoai, count(products.name) as tongSP')
+        //             ->join('categories','categories.id','=','products.id_type')
+        //             ->where('categories.name', 'like', '%Macbook%')
+        //             ->groupBy('categories.name')
+        //             ->havingRaw('count(products.name) >=5')
+        //             ->get();
+        //$products = DB::table('products')->whereBetween('price',[50000000,100000000])->get();
+        // $products = DB::table('products')
+        //             ->where('name','iPhone X 256GB')
+        //             ->orWhere('name','iPhone 8 Plus 256GB')
+        //             ->orWhere('name','iPhone 7 Plus 32GB')     
+        //             ->orderBy()
+        //             ->orderBy()               
+        //             ->get();
+        // $products = DB::table('products')
+        //             ->selectRaw('categories.name as TenLoai, count(products.name) as tongSP')
+        //             ->join('categories','categories.id','=','products.id_type')
+        //             ->where('categories.name', '=', 'Phu kien')
+        //             ->orWhere('categories.name', '=', 'iMac')
+        //             ->groupBy('categories.name')
+        //             ->havingRaw('count(products.name) >=10')
+        //             ->orderBy('tongSP', 'ASC')
+        //             ->get();
+    
         $products = DB::table('products')
-                    ->selectRaw('categories.name as TenLoai, count(products.name) as tongSP')
+                    ->selectRaw('categories.name as TenLoai, avg(products.price) as DGTB, min(products.price) as minPrice, max(products.price) as maxPrice')
                     ->join('categories','categories.id','=','products.id_type')
                     ->groupBy('categories.name')
-                    ->havingRaw('count(products.name) >=5')
                     ->get();
-
         dd($products);
     }
 }
